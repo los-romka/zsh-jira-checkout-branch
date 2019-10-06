b244848d9e045641401a54d73f22bbe003b4d0e2
los-romka <sin_ss@list.ru>
2019-10-06 23:52:39 +1000

    Inital commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# zsh-jira-checkout-branch
+> Zsh plugin for easy navigation between git branches associated with JIRA issues
+
+This is written as a zsh plugin, it also works with bash if you follow the [manual installation instructions](#manually).
+
+## Installation
+
+### As an [Oh My ZSH!](https://github.com/robbyrussell/oh-my-zsh) custom plugin
+
+Clone `zsh-jira-checkout-branch` into your custom plugins repo
+
+```shell
+git clone https://github.com/los-romka/zsh-jira-checkout-branch ~/.oh-my-zsh/custom/plugins/zsh-jira-checkout-branch
+```
+Then load as a plugin in your `.zshrc`
+
+```shell
+plugins+=(zsh-jira-checkout-branch)
+```
+
+Keep in mind that plugins need to be added before `oh-my-zsh.sh` is sourced.
+
+Then [define required JIRA variables](#Required)
+
+### Manually
+Clone this repository somewhere (`~/.zsh-jira-checkout-branch` for example)
+
+```shell
+git clone https://github.com/los-romka/zsh-jira-checkout-branch.git ~/.zsh-jira-checkout-branch
+```
+Then source it in your `.zshrc` (or `.bashrc`)
+
+```shell
+source ~/.zsh-jira-checkout-branch/zsh-jira-checkout-branch.plugin.zsh
+```
+
+Then [define required JIRA variables](#Required)
+
+### Required
+
+Put `$JIRA_USER:$JIRA_PASS` into `~/.jira-credentials`
+
+```shell
+echo "user:pass" > `~/.jira-credentials`
+```
+
+Define `JIRA_API_URL` in your `.zshrc` (or `.bashrc`)
+
+```shell
+echo -e "JIRA_API_URL=https://jira.mysite.com/rest/api/2/" >> ~/.zshrc
+```
+
+## Usage
+
+Type `jco` in your Git-repository to checkout between branches
+
+## Options
+
+### Custom Directory
+
+You can specify a custom `JIRA_CREDENTIALS_PATH` in your `.zshrc` (or `.bashrc`)
+
+```shell
+echo -e "JIRA_CREDENTIALS_PATH=https://jira.mysite.com/rest/api/2/" >> ~/.zshrc
+```
+
+## Dependencies
+
+- [`JIRA-server`](https://www.atlassian.com/software/jira) - The #1 software development tool used by agile teams
+- [`git`](https://www.git-scm.com/) - U know it
+- [`fzf`](https://github.com/junegunn/fzf) - General-purpose command-line fuzzy finder.
+
+## License
+
+MIT © Roman Los
---
+++ zsh-jira-checkout-branch.zsh
@@ -0,0 +1,101 @@
+jco() {
+  # global JIRA_API_URL
+  # global JIRA_CREDENTIALS_PATH
+
+  JIRA_CREDENTIALS_PATH=${JIRA_CREDENTIALS_PATH:-~/.jira-credentials}
+
+  local JIRA_ISSUES_CACHE_PATH=/tmp/.jira_issues.cache
+  local JIRA_ISSUES_CACHE_TTL=60
+  local JIRA_BRANCHES_CACHE_PATH=/tmp/.jira_branches.cache
+  local JIRA_BRANCHES_CACHE_TTL=60
+  local JIRA_CREDENTIALS
+
+  [ "$(git symbolic-ref --short HEAD)" ] || return
+
+  [ -f "$JIRA_CREDENTIALS_PATH" ] && [ -z $JIRA_CREDENTIALS ] && JIRA_CREDENTIALS=$(cat "$JIRA_CREDENTIALS_PATH" | xargs)
+  [ -z $JIRA_CREDENTIALS ] && echo 'Put "$JIRA_USER:$JIRA_PASS" into '$JIRA_CREDENTIALS_PATH && return
+  [ -z $JIRA_API_URL ] && echo 'Define $JIRA_API_URL (Example: "https://jira.mysite.com/rest/api/2/")' && return
+
+  git_branches() {
+    local branches=$(
+      git --no-pager branch --all \
+            --sort=-committerdate \
+            --format='%(refname:short)|%(committerdate:relative)|%(authorname)' \
+            --color=always \
+          | grep --color=never -vE "$(git --no-pager branch --format='%(refname:short)' | sed 's/^/origin\//g' | xargs | tr ' ' '|')" \
+          | sed '/^$/d'
+    )
+    echo "$branches"
+  }
+
+  jira_issues() {
+    local CACHE_FILE="$JIRA_ISSUES_CACHE_PATH"
+
+    [ -f "$CACHE_FILE" ] && find "$CACHE_FILE" -type f -mmin +"$JIRA_ISSUES_CACHE_TTL" -print0 | xargs -0 rm -f
+    [ -f "$CACHE_FILE" ] && (( $(du -k "$CACHE_FILE" | cut -f1) > 10 )) && cat "$CACHE_FILE" && return
+
+    local issueKeys=$(git_branches | sed 's/origin\///g' | cut -d"|" -f1 | grep --color=never -iE '^[A-Z]+-[0-9]+$' ) || return
+    local maxResults=$(echo $issueKeys | wc -l)
+
+    local searchUrl=$(echo "${JIRA_API_URL}search?fields=summary&maxResults=${maxResults}&jql=issueKey%20in%20($(echo -e $issueKeys | xargs | tr ' ' ','))") || return
+    local issues=$(curl --fail -L -u "$JIRA_CREDENTIALS" "$searchUrl" | jq -r '.issues | map(.key + "|" + .fields.summary) | join("\n")') || return
+
+    echo "$issues" > "$CACHE_FILE"
+    cat "$CACHE_FILE"
+  }
+
+  jira_branches() {
+    local CACHE_FILE="$JIRA_BRANCHES_CACHE_PATH"
+
+    [ -f "$CACHE_FILE" ] && find "$CACHE_FILE" -type f -mmin +"$JIRA_BRANCHES_CACHE_TTL" -print0 | xargs -0 rm -f
+    [ -f "$CACHE_FILE" ] && (( $(du -k "$CACHE_FILE" | cut -f1) > 10 )) && cat "$CACHE_FILE" && return
+
+    local branches=$(git_branches) || return
+    local issues=$(jira_issues) || return
+
+    (( $(echo "$branches" | wc -c) < 2 )) && return
+    (( $(echo "$issues" | wc -c) < 2 )) && return
+
+    local choices=$(
+      echo "$branches" | while read branchInfo ; do
+        local branch=$(echo $branchInfo | cut -d"|" -f1)
+        local issueInfo=$(echo $issues | grep --color=never -iE $(echo "^$branch" | sed 's/origin\///g' ) )
+
+        if [ "$issueInfo" != '' ]; then
+          local title=$(echo $issueInfo | cut -d"|" -f2-)
+          echo "$branchInfo|$title"
+        fi
+      done 
+    ) || return
+
+    (( $(echo "$choices" | wc -c) < 2 )) && return
+
+    local branches=$(
+      echo "$choices" | while read choice ; do
+        local branch=$(echo $choice | cut -d"|" -f1)
+        local timeAgo=$(echo $choice | cut -d"|" -f2)
+        local author=$(echo $choice | cut -d"|" -f3)
+        local title=$(echo $choice | cut -d"|" -f4)
+
+        echo "$(tput setaf 3)$branch|$(tput setaf 5)$author|$(tput setaf 2)$timeAgo|$(tput sgr0)$title"
+      done | column -ts'|'
+    ) || return
+
+    echo "$branches" > "$CACHE_FILE"
+    cat "$CACHE_FILE"
+  }
+
+  local branches=$(jira_branches) || return
+
+  [ "$branches" = "" ] && return
+
+  local target=$(
+    (echo "$branches";) |
+    fzf --no-hscroll --no-multi \
+        --ansi --preview="git --no-pager log -150 --pretty=format:%s '..{1}'") || return
+
+  git checkout $(awk '{print $1}' <<<"$target" | sed 's/origin\///g' )
+}
+
+# Set $?.
+true
