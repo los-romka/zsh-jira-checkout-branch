18c3eec6b8f1bf6f4b4c7623bbfab0895e03fa50
los-romka <sin@drom.ru>
2019-10-16 17:02:11 +1000

    Small refactoring

--- zsh-jira-checkout-branch.zsh
+++ zsh-jira-checkout-branch.zsh
@@ -1,41 +1,44 @@
-jco() {
-  # global JIRA_API_URL
-  # global JIRA_CREDENTIALS_PATH
+_zsh_jira_checkout_branch_init() {
+    # global JIRA_API_URL
+    # global JIRA_CREDENTIALS_PATH
 
-  JIRA_CREDENTIALS_PATH=${JIRA_CREDENTIALS_PATH:-~/.jira-credentials}
+    local ERROR_CODE=127
+    local GIT_REPOSITORY_HASH
 
-  [ "$(git symbolic-ref --short HEAD)" ] || return
+    JIRA_CREDENTIALS_PATH=${JIRA_CREDENTIALS_PATH:-~/.jira-credentials}
 
-  local GIT_REPOSITORY_HASH=$(git remote -v | shasum -p | cut -d' ' -f1)
+    [ "$(git symbolic-ref --short HEAD)" ] || return $ERROR_CODE
 
-  local JIRA_ISSUES_CACHE_PATH=/tmp/.jira_issues.$GIT_REPOSITORY_HASH.cache
-  local JIRA_ISSUES_CACHE_TTL=60
-  local JIRA_BRANCHES_CACHE_PATH=/tmp/.jira_branches.$GIT_REPOSITORY_HASH.cache
-  local JIRA_BRANCHES_CACHE_TTL=60
-  local JIRA_CREDENTIALS
+    GIT_REPOSITORY_HASH=$(git remote -v | shasum -p | cut -d' ' -f1)
 
-  [ -f "$JIRA_CREDENTIALS_PATH" ] && [ -z "$JIRA_CREDENTIALS" ] && JIRA_CREDENTIALS=$(cat "$JIRA_CREDENTIALS_PATH" | xargs)
-  [ -z "$JIRA_CREDENTIALS" ] && echo Put "$JIRA_USER:$JIRA_PASS" into "$JIRA_CREDENTIALS_PATH" && return
-  [ -z "$JIRA_API_URL" ] && echo Define \$JIRA_API_URL \(Example: "https://jira.mysite.com/rest/api/2/"\) && return
+    _JIRA_ISSUES_CACHE_PATH=/tmp/.jira_issues.$GIT_REPOSITORY_HASH.cache
+    _JIRA_ISSUES_CACHE_TTL=60
 
-  git_branches() {
-      git --no-pager branch --all \
-            --sort=-committerdate \
-            --format='%(refname:short)|%(committerdate:relative)|%(authorname)' \
-            --color=always \
-          | grep -a --color=never -vE "$(git --no-pager branch --format='%(refname:short)' | sed 's/^/origin\//g' | xargs | tr ' ' '|')" \
-          | sed '/^$/d'
-  }
+    [ -f "$JIRA_CREDENTIALS_PATH" ] && [ -z "$_JIRA_CREDENTIALS" ] && _JIRA_CREDENTIALS=$(cat "$JIRA_CREDENTIALS_PATH" | xargs)
+    [ -z "$_JIRA_CREDENTIALS" ] && echo Put "$JIRA_USER:$JIRA_PASS" into "$JIRA_CREDENTIALS_PATH" && return $ERROR_CODE
+    [ -z "$JIRA_API_URL" ] && echo Define \$JIRA_API_URL \(Example: "https://jira.mysite.com/rest/api/2/"\) && return $ERROR_CODE
 
-  jira_issues() {
-    local CACHE_FILE="$JIRA_ISSUES_CACHE_PATH"
+    return 0
+}
 
-    [ -f "$CACHE_FILE" ] && find "$CACHE_FILE" -type f -mmin +"$JIRA_ISSUES_CACHE_TTL" -print0 | xargs -0 rm -f
+_zsh_jira_checkout_branch_git_all_branches() {
+    git --no-pager branch --all \
+          --sort=-committerdate \
+          --format='%(refname:short)|%(committerdate:relative)|%(authorname)' \
+          --color=always \
+        | grep -a --color=never -vE "$(git --no-pager branch --format='%(refname:short)' | sed 's/^/origin\//g' | xargs | tr ' ' '|')" \
+        | sed '/^$/d'
+}
+
+_zsh_jira_checkout_branch_jira_issues() {
+    local CACHE_FILE="$_JIRA_ISSUES_CACHE_PATH"
+
+    [ -f "$CACHE_FILE" ] && find "$CACHE_FILE" -type f -mmin +"$_JIRA_ISSUES_CACHE_TTL" -print0 | xargs -0 rm -f
     [ -f "$CACHE_FILE" ] && (( $(du -k "$CACHE_FILE" | cut -f1) > 3 )) && cat "$CACHE_FILE" && return
 
     local issueKeys maxResults searchUrl issues
 
-    issueKeys=$(git_branches | sed 's/origin\///g' | cut -d"|" -f1 | grep -a --color=never -iE '^[A-Z]+-[0-9]+$' ) || return
+    issueKeys=$(_zsh_jira_checkout_branch_git_all_branches | sed 's/origin\///g' | cut -d"|" -f1 | grep -a --color=never -iE '^[A-Z]+-[0-9]+$' ) || return
     maxResults=$(echo "$issueKeys" | wc -l | xargs)
 
     if [ "$issueKeys" = "" ]; then
@@ -47,23 +50,23 @@ jco() {
 
     >&2 echo "Requesting JIRA issues"
 
-    issues=$(curl --fail -L -u "$JIRA_CREDENTIALS" "$searchUrl" | jq -r '.issues | map(.key + "|" + .fields.summary) | join("\n")') || return
+    issues=$(curl --fail -L -u "$_JIRA_CREDENTIALS" "$searchUrl" | jq -r '.issues | map(.key + "|" + .fields.summary) | join("\n")') || return
 
     echo "$issues" > "$CACHE_FILE"
     cat "$CACHE_FILE"
-  }
+}
 
-  jira_branches() {
+_zsh_jira_checkout_branch_add_jira_titles() {
     local issues branch timeAgo author title branchName
 
     declare -A issues
 
-    jira_issues | while read issueInfo ; do
+    _zsh_jira_checkout_branch_jira_issues | while read issueInfo ; do
         branch=$(echo "$issueInfo" | cut -d"|" -f1)
         issues["$branch"]=$(echo "$issueInfo" | cut -d"|" -f2-)
     done
 
-    git_branches | while read choice ; do
+    while read choice; do
         branch=$(echo "$choice" | cut -d"|" -f1)
         branchName=$(echo "$branch" | sed 's/origin\///g')
         timeAgo=$(echo "$choice" | cut -d"|" -f2)
@@ -73,10 +76,17 @@ jco() {
         printf "$(tput setaf 3)%-35s $(tput setaf 5)%-25s $(tput setaf 2)%-20s $(tput sgr0)%s\n" \
             "$branch" "$author" "$timeAgo" "$title"
     done
-  }
+}
+
+jco() {
+  local _JIRA_ISSUES_CACHE_PATH
+  local _JIRA_ISSUES_CACHE_TTL
+  local _JIRA_CREDENTIALS
+
+  _zsh_jira_checkout_branch_init || return
 
   local target=$(
-    jira_branches |
+    _zsh_jira_checkout_branch_git_all_branches | _zsh_jira_checkout_branch_add_jira_titles |
     fzf --no-hscroll --no-multi \
         --preview-window up:10 \
         --no-sort \
